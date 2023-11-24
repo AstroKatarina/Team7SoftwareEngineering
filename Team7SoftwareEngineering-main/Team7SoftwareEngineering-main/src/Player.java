f2d45293ddce00df61b041054ff5eae02792a1ee
ChrisStites <145061894+ChrisStites@users.noreply.github.com>
2023-11-24 12:14:47 -0500

    Add files via upload

---
+++ Team7SoftwareEngineering-main/Team7SoftwareEngineering-main/src/Player.java
@@ -0,0 +1,39 @@
+class Player
+{
+    int ID;
+    String CodeName;
+    int Score;
+    int Team;
+    // Green==0 red==1
+    int EquipmentID;
+
+    Player()
+    {
+        ID = 0;
+        CodeName = "";
+        Score = 0;
+        Team = 0;
+        EquipmentID = 0;
+    }
+
+    Player(int id, String codename, int team, int equipmentid)
+    {
+        ID = id;
+        CodeName = codename;
+        Score = 0;
+        Team = team;
+        EquipmentID = equipmentid;
+    }
+
+
+    void PlayerReset()
+    {
+        ID = 0;
+        CodeName = "";
+        Score = 0;
+        Team = 0;
+        EquipmentID = 0;
+    }
+
+
+}
