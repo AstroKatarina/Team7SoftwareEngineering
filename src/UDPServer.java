ed0f484ca4c30a752ead1d61f41e51cdc2e4b259
Evan Bonar <98934102+EvanMBonar@users.noreply.github.com>
2023-11-26 11:57:16 -0600

    Debugging

--- src/UDPServer.java
+++ src/UDPServer.java
@@ -56,6 +56,7 @@ public class UDPServer implements Runnable{
 
     @Override
     public void run() {
+        System.out.println("Thread about to start server\n");
         startServer(); // Start the server when this thread starts running
     }
 }
